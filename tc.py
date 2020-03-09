e065111f23b8be9ecc07fc6c537649098162ffb9
Ardi <ardier@outlook.com>
2020-03-08 19:46:08 -0700

    Fixing some bugs. Added the generate_dominator_set function that takes a mapping of mutants to tests they kill and generates a minimal set of dominator mutants.

--- tc.py
+++ tc.py
@@ -3,19 +3,23 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def csv_reader(csv_filename):
-    """This function reads CSV files containing killMaps.
+def read_csv(csv_filename):
+    """This function reads CSV files containing killMaps and outputs the killMap as a dictionary of mutants to a set of
+     tests that fail when that mutant is presented..
 
-        Parameters:
-            csv_filename (Path): The killmap passed in as a CSV file.
+    Parameters:
+        csv_filename (Path): The killMap passed in as a CSV file.
 
-        Returns:
-            completeness_in_reader: A dictionary that of mutants to tests that fail when that mutant is presented.
-                """
+    Returns:
+        completeness_in_reader: A dictionary of mutants to a set of tests that fail when that mutant is presented.
+    """
+
+    # TODO fix assert
     assert csv_filename.__contains__(".csv"), 'Invalid input'
 
     # begin reading the file
     with open(csv_filename, newline='') as File:
+        # Create an empty dictionary
         completeness_in_reader = {}
         reader = csv.reader(File)
 
@@ -31,51 +35,71 @@ def csv_reader(csv_filename):
         return completeness_in_reader
 
 
-def kill_count(completeness):
-    """Count how many tests are killed for a presented mutant.
+# Some global variable - may change generate_plot_points to return a tuple instead in the future
+# A list of tests explored thus far
+tests_explored = set()
+
+# A minimal list of mutants as tuples that maps mutants to the number of unique tests they kill
+sorted_count_non_duplicate = []
+
+
+def count_kill(completeness):
+    """Counts how many tests are killed for a presented mutant, and sorts mutants by the number of tests they kill.
 
     Parameters:
         completeness: A dictionary of mutants to tests that fail when that mutant is presented.
 
     Returns:
-        unsorted_weight: An unsorted dictionary that maps mutants to the number tests the fail for
+        count_sorted: A sorted list of tuples that stores mutants and the number of tests they kill in descending order
 
             """
-    unsorted_weight = {}
+    unsorted_count = {}
     for size in completeness:
         s = len(completeness.get(size))
-        unsorted_weight[size] = s
-    return unsorted_weight
+        unsorted_count[size] = s
+    count_sorted = sorted(unsorted_count.items(), key=lambda w: w[1], reverse=True)
+    return count_sorted
 
 
-def sorter(unsorted_weight):
-    """Sorts mutants by the number of tests they kill.
+def create_minimal_dominator_set_to_count_mapping(sorted_count, completeness):
+    """Creates a minimal list of tuples that maps dominator mutants to the number of tests they kill
+        The mutants that kill the same set of tests or a smaller subset of tests are omitted in this list
 
     Parameters:
-        unsorted_weight: A dictionary of mutants to the number of tests that fail when that mutant is presented.
+        sorted_count: A sorted list of tuples that stores mutants and the number of tests they kill in descending order
+        completeness: An unsorted dictionary that maps mutants to the number tests the fail for
 
     Returns:
-        weight_sorted: A sorted list of tuples that stores mutants and the number of tests they kill in descending order
+        sorted_count_non_duplicate: A minimal list of mutants as tuples that maps mutants to the number of
+        unique tests they kill
 
             """
-    weight_sorted = sorted(unsorted_weight.items(), key=lambda w: w[1], reverse=True)
-    return weight_sorted
+    global sorted_count_non_duplicate
+    global tests_explored
+    for x in sorted_count:
+
+        test_name = x[0]
+        failing_test = completeness.get(test_name)
+
+        if failing_test:
+
+            # remove test that are already detected
+            failing_test = failing_test - tests_explored
+            if failing_test:
+                sorted_count_non_duplicate.append((test_name, len(failing_test)))
+
+                tests_explored = tests_explored | failing_test
+    sorted_count_non_duplicate = sorted(sorted_count_non_duplicate, key=lambda u: -u[1])
+    tests_explored = sorted(tests_explored)
+    return sorted_count_non_duplicate
 
 
-# Some global variable - may change duplicate_remover to return a tuple instead in the future
-# A list of tests explored thus far
-tests_explored = set()
-
-# A minimal list of mutants as tuples that maps mutants to the number of unique tests they kill
-sorted_weight_non_duplicate = []
-
-
-def duplicate_remover(sorted_weight_non_duplicate_remover):
+def generate_plot_points(sorted_count_non_duplicate_remover):
     """removes tests that are already killed by a mutant that has a larger set of tests
         from the remaining(smaller or equal) mutants' set of tests
 
     Parameters:
-        sorted_weight_non_duplicate_remover: A minimal list of mutants as tuples that maps mutants to the number of
+        sorted_count_non_duplicate_remover: A minimal list of mutants as tuples that maps mutants to the number of
         unique tests they kill
 
     Returns:
@@ -87,77 +111,63 @@ def duplicate_remover(sorted_weight_non_duplicate_remover):
     summer = 0
 
     # iterate between two tuples to gather information
-    for i, test in zip(range(len(sorted_weight_non_duplicate_remover)), sorted_weight_non_duplicate_remover):
+    for i, test in zip(range(len(sorted_count_non_duplicate_remover)), sorted_count_non_duplicate_remover):
         summer += test[1]
         current_test_completeness.append([i + 1, summer])
     return current_test_completeness
 
 
-def dominator_setter(sorted_weight, completeness):
-    """Creates a minimal list of tuples that maps dominator mutants to the number of tests they kill
-        The mutants the kill the same set of tests or a smaller subset of tests are omitted in this list
-
-    Parameters:
-        sorted_weight: A sorted list of tuples that stores mutants and the number of tests they kill in descending order
-        completeness: An unsorted dictionary that maps mutants to the number tests the fail for
-
-    Returns:
-        sorted_weight_non_duplicate: A minimal list of mutants as tuples that maps mutants to the number of
-        unique tests they kill
-
-            """
-    global sorted_weight_non_duplicate
-    global tests_explored
-    for x in sorted_weight:
-
-        test_name = x[0]
-        failing_test = completeness.get(test_name)
-
-        if failing_test:
-
-            # remove test that are already detected
-            failing_test = failing_test - tests_explored
-            if failing_test:
-                sorted_weight_non_duplicate.append((test_name, len(failing_test)))
-                tests_explored = tests_explored | failing_test
-
-    sorted_weight_non_duplicate = sorted(sorted_weight_non_duplicate, key=lambda u: -u[1])
-    tests_explored = sorted(tests_explored)
-    return sorted_weight_non_duplicate
-
-
-def dominator_export(sorted_weight_non_duplicate):
+def export_dominator_set_to_count_mapping_to_csv(sorted_count_non_duplicate):
     """This function writes a CSV file.
 
     Parameters:
-        sorted_weight_non_duplicate: A minimal list of mutants as tuples that maps mutants to the number of
+        sorted_count_non_duplicate: A minimal list of mutants as tuples that maps mutants to the number of
         unique tests they kill
 
                     """
     with open('data/dominator_set.csv', 'w', newline='') as csvfile:
         wr = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
         wr.writerow(['Mutant No', 'Number of tests'])
-        for x, y in sorted_weight_non_duplicate:
+        for x, y in sorted_count_non_duplicate:
             wr.writerow([x, y])
 
 
-def plot(completeness, sorted_weight_non_duplicate, mutants_explored):
+def generate_dominator_set(completeness_in_reader):
+    """Calculates a dominating set of mutants.
+    Parameters:
+        kill_map: A mapping from test identifiers to a set of identifiers for
+            mutants killed.
+    Returns:
+        The set of identifiers of mutants in a dominating set.
+    """
+    # TODO(ardier): Extend this docstring with details about how it resolves
+    #  indistinguishable mutants.
+    test_sorted_count_main = count_kill(completeness_in_reader)
+    dominator_set_main1 = create_minimal_dominator_set_to_count_mapping(test_sorted_count_main, completeness_in_reader)
+    dominator_set = []
+    for x in dominator_set_main1:
+        dominator_set.append(x[0])
+    return dominator_set
+
+
+def plot(test_completeness_no_duplicates, sorted_count_non_duplicate, tests_explored):
     """plots test completes achieved on the y axis for for each unit of work which is a (dominator) mutant presented,
     where mutants are sorted by the number of unique tests they kill
 
     Parameters:
-        completeness: A sorted list of tuples that stores mutants and the number of tests they kill in descending order
-        sorted_weight_non_duplicate: A minimal list of mutants as tuples that maps mutants to the number of
+        test_completeness_no_duplicates: A sorted list of tuples that stores mutants and the number of tests
+        they kill in descending order
+        sorted_count_non_duplicate: A minimal list of mutants as tuples that maps mutants to the number of
         unique tests they kill
-        mutants_explored: A list of all mutants explored
+        tests_explored: A list of all mutants explored
 
             """
     plotter = pd.DataFrame(
-        data=completeness,
+        data=test_completeness_no_duplicates,
         columns=["Work", "Test Completeness"]
     )
-    ax = plotter.plot(x='Work', y='Test Completeness', xticks=range(len(sorted_weight_non_duplicate)),
-                      yticks=range(0, len(mutants_explored), 100), legend=False)
+    ax = plotter.plot(x='Work', y='Test Completeness', xticks=range(len(sorted_count_non_duplicate)),
+                      yticks=range(0, len(tests_explored), 100), legend=False)
     ax.set_ylabel("Test Completeness")
     plt.show()
 
@@ -165,11 +175,14 @@ def plot(completeness, sorted_weight_non_duplicate, mutants_explored):
 # putting it altogether
 if __name__ == "__main__":
     filename = 'test-data/killMap.csv'
-    completeness_main = csv_reader(filename)
-    unsorted_weights_main = kill_count(completeness_main)
-    test_sorted_weight_main = sorter(unsorted_weights_main)
-    dominator_set_main = dominator_setter(test_sorted_weight_main, completeness_main)
+    completeness_main = read_csv(filename)
+    print(completeness_main)
+    test_sorted_count_main = count_kill(completeness_main)
+    dominator_set_main = create_minimal_dominator_set_to_count_mapping(test_sorted_count_main, completeness_main)
     print(dominator_set_main)
-    test_completeness_main = duplicate_remover(dominator_set_main)
-    dominator_export(sorted_weight_non_duplicate)
-    plot(test_completeness_main, sorted_weight_non_duplicate, tests_explored)
+    test_completeness_no_duplicates = generate_plot_points(dominator_set_main)
+    print(test_completeness_no_duplicates)
+    export_dominator_set_to_count_mapping_to_csv(sorted_count_non_duplicate)
+    dominator_set = generate_dominator_set(completeness_main)
+    print(dominator_set)
+    plot(test_completeness_no_duplicates, sorted_count_non_duplicate, tests_explored)
--- test_tc.py
+++ test_tc.py
@@ -1,5 +1,7 @@
+from _ast import Assert
 from unittest import TestCase
-
+import cv2
+import numpy as np
 import tc
 
 
@@ -11,26 +13,26 @@ class Test(TestCase):
                        """
 
         filename = "test-data/test.csv"
-        result = tc.csv_reader(filename)
+        result = tc.read_csv(filename)
         print(result)
 
     def test_csv_reader_empty_input(self):
-        """This function tests whether the csv_reader can handle invalid input.
+        """This function tests whether the read_csv can handle invalid input.
 
                        """
 
         filename = "nonsense"
         with self.assertRaises(Exception) as context:
-            tc.csv_reader(filename)
+            tc.read_csv(filename)
 
         self.assertTrue('Invalid input' in str(context.exception))
 
     def test_csv_reader_basic_results_check(self):
-        """This function tests whether the csv_reader outputs a valid dictionary.
+        """This function tests whether the read_csv outputs a valid dictionary.
 
                                """
         filename = "test-data/test.csv"
-        result = tc.csv_reader(filename)
+        result = tc.read_csv(filename)
         expected_result = {1: {1, 2}, 2: {1, 2}, 3: {1, 3}, 4: {4}}
         self.assertEqual(expected_result, result)
 
@@ -39,16 +41,16 @@ class Test(TestCase):
 
                                """
         test_input = {1: {1, 2}, 2: {1, 2}, 3: {1, 3}, 4: {4}}
-        result = tc.kill_count(test_input)
+        result = tc.count_kill(test_input)
         expected_result = {1: 2, 2: 2, 3: 2, 4: 1}
         self.assertEqual(expected_result, result)
 
     def test_sorter(self):
-        """This function tests whether the sorter outputs a valid list of tuples.
+        """This function tests whether the sort_ outputs a valid list of tuples.
 
                                """
         test_input = {1: 2, 2: 2, 3: 2, 4: 1}
-        result = tc.sorter(test_input)
+        result = tc.sort_(test_input)
         expected_result = [(1, 2), (2, 2), (3, 2), (4, 1)]
         self.assertEqual(expected_result, result)
 
@@ -58,4 +60,8 @@ class Test(TestCase):
         test_input = ([[0, 0], [1, 2], [2, 3], [3, 4]])
         test_input2 = {1: {1, 2}, 2: {1, 2}, 3: {1, 3}, 4: {4}}
         test_input3 = [1, 2, 3, 4]
-        tc.plot(test_input, test_input2, test_input3)
+
+        # Assert
+        # open("test-data/test-1.png", "rb").read() == tc.plot(test_input, test_input2, test_input3),
+        # 'The output is invalid'
+        # actual = cv2.imread(tc.plot(test_input, test_input2, test_input3))
