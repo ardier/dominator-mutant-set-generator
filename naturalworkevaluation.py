da2fc0e81d63b00835962acad5ce4baf63b9e515
ardier <36089652+ardier@users.noreply.github.com>
2020-08-14 14:56:25 -0700

    Almost done with creating mappings from the file

--- naturalworkevaluation.py
+++ naturalworkevaluation.py
@@ -29,30 +29,54 @@ def generate_natural_work_evaluation(results_dir):
 def generate_mutant_to_token_mapping(log_file):
     mutant_to_token_mapping: Optional[Dict[int, int[-1, -1]]] = dict()
     tokenization_pattern = re.compile(
-        r"([0-9]*);[A-Z]*;([0-9]*);(([0-9]*)[A-Z]*)")
+        r"([0-9]*);[A-Z]*;([0-9]*);((([0-9]*)[ ]*[a-z]*[A-Z]*[.]*[:]*[-]*[_]*[\"]*[']*)*)")
     with open(log_file, 'r') as fo:
         for line in fo:
-
             header_match = tokenization_pattern.match(line)
             if header_match:
                 subtoken = -1
-                if header_match.group(3) and str(header_match.group(3)) != \
-                        "0L":
-                    subtoken = int(header_match.group(3))
+                if header_match.group(3):
+                    subtoken = header_match.group(3)
                 mutant_to_token_mapping[int(header_match.group(1))] = [
                     int(header_match.group(2)), subtoken]
             continue
             raise ValueError("No pattern matched line: {}".format(line))
+        # TODO remove when done
+        # for x in mutant_to_token_mapping:
+        #    print(str(x)+" "+str(mutant_to_token_mapping[x]))
         return mutant_to_token_mapping
 
 
 def generate_scores(mutant_token_mapping, mml_csv):
+    token_prob_position_score: Optional[Dict[int[-1, -1], int[-1, -1]]] = dict()
+    score_reading_pattern = re.compile(
+        r"[A-Z_(]*([\d]*)[)]<([\d]*[\"]*[']*[ ]*[a-z]*[A-Z]*[.]*[:]*["
+        r"\_]*[(]*[)]*[>]*)*-> ([\"'\d*[a-z]]*[.a-zA-Z0-9_\"',<> $?\[\]:("
+        r")\\-]*)#\d.\d*([A-Z]-[0-9])*#(\d.\d*)([A-Z]-[0-9])*#[a-z]*#("
+        r"\d.\d*)([A-Z]-[0-9])*#[a-z]*#[a-z]*#(\d.\d*([A-Z]-[0-9])*)*")
+
     with open(mml_csv, 'r') as fo:
         for line in fo:
-            print(line)
+            if line.strip() == "":
+                continue
+            header2_match = score_reading_pattern.match(line)
+            # group 1 -> token number identifier
+            # group 3 -> subtoken number identifier
+            # group 5 -> ngramProb
+            # group 9 -> positionscore
+            # mutant_scores_mapping: {[token, subtoken]: []}
+            if header2_match:
+                token_prob_position_score[int(header2_match.group(1)),
+                                          header2_match.group(3)[1:-1]] = \
+                    [float(header2_match.group(5)), float(
+                        header2_match.group(9))]
             continue
             raise ValueError("No pattern matched line: {}".format(line))
 
+    # TODO create the actual mapping
+    mutant_prob_position_score: Optional[
+        Dict[int[-1, -1], int[-1, -1]]] = dict()
+    return mutant_prob_position_score
 
 if __name__ == "__main__":
     generate_natural_work_evaluation("..\\results")
